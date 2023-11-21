684d0ff709f0ae4cad6f5d538d9a16dd99b4ac05
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-11-20 23:42:33 -0300

    add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
-import adapter from 'svelte-adapter-bun';
+// import adapter from 'svelte-adapter-bun';
+import adapter from '@sveltejs/adapter-vercel';
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
